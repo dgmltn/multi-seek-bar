9b9f64b79630c05f1cd83acf5851e0abedb7b543
Doug Melton <doug@mobiata.com>
2015-11-26 20:58:36 -0800

    Added ability to set start, sweep angles on ArcSlider

--- slider/src/main/java/com/dgmltn/slider/ArcSlider.java
+++ slider/src/main/java/com/dgmltn/slider/ArcSlider.java
@@ -1,11 +1,13 @@
 package com.dgmltn.slider;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.dgmltn.slider.internal.AbsSlider;
 import com.dgmltn.slider.internal.ArcUtils;
@@ -15,23 +17,31 @@ import com.dgmltn.slider.internal.ArcUtils;
  */
 public class ArcSlider extends AbsSlider {
 
-	private static final float ARC_START = 150f;
-	private static final float ARC_SWEEP = 240f;
-
 	private PointF mCenter = new PointF();
 	private float mRadius = 1f;
 	private RectF mBounds = new RectF();
+	private PointF mTmpPointF = new PointF();
+
+	private int mArcStart = 150;
+	private int mArcSweep = 240;
 
 	public ArcSlider(Context context, AttributeSet attrs) {
 		super(context, attrs);
+
+		if (attrs != null) {
+			TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ArcSlider, 0, 0);
+			mArcStart = ta.getInt(R.styleable.ArcSlider_arc_start, mArcStart);
+			mArcSweep = ta.getInt(R.styleable.ArcSlider_arc_sweep, mArcSweep);
+			ta.recycle();
+		}
 	}
 
 	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged(w, h, oldw, oldh);
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
 
-		float pw = w - getPaddingLeft() - getPaddingRight();
-		float ph = h - getPaddingTop() - getPaddingBottom();
+		float pw = getWidth() - getPaddingLeft() - getPaddingRight();
+		float ph = getHeight() - getPaddingTop() - getPaddingBottom();
 
 		calculateBounds();
 
@@ -46,20 +56,36 @@ public class ArcSlider extends AbsSlider {
 		mBounds.offset(dx, dy);
 	}
 
+	public int getArcStart() {
+		return mArcStart;
+	}
+
+	public void setArcStart(int mArcStart) {
+		this.mArcStart = mArcStart;
+		requestLayout();
+	}
+
+	public int getArcSweep() {
+		return mArcSweep;
+	}
+
+	public void setArcSweep(int mArcSweep) {
+		this.mArcSweep = mArcSweep;
+		requestLayout();
+	}
+
 	private void calculateBounds() {
-		PointF test = new PointF();
+		getNearestPointOnBar(mTmpPointF, mCenter.x - mRadius, mCenter.y);
+		mBounds.set(mTmpPointF.x, mTmpPointF.y, mTmpPointF.x, mTmpPointF.y);
 
-		getNearestPointOnBar(test, mCenter.x - mRadius, mCenter.y);
-		mBounds.set(test.x, test.y, test.x, test.y);
+		getNearestPointOnBar(mTmpPointF, mCenter.x + mRadius, mCenter.y);
+		mBounds.union(mTmpPointF.x, mTmpPointF.y);
 
-		getNearestPointOnBar(test, mCenter.x + mRadius, mCenter.y);
-		mBounds.union(test.x, test.y);
+		getNearestPointOnBar(mTmpPointF, mCenter.x, mCenter.y - mRadius);
+		mBounds.union(mTmpPointF.x, mTmpPointF.y);
 
-		getNearestPointOnBar(test, mCenter.x, mCenter.y - mRadius);
-		mBounds.union(test.x, test.y);
-
-		getNearestPointOnBar(test, mCenter.x, mCenter.y + mRadius);
-		mBounds.union(test.x, test.y);
+		getNearestPointOnBar(mTmpPointF, mCenter.x, mCenter.y + mRadius);
+		mBounds.union(mTmpPointF.x, mTmpPointF.y);
 	}
 
 	private void getNearestPointOnBar(PointF out, float x, float y) {
@@ -68,27 +94,27 @@ public class ArcSlider extends AbsSlider {
 
 	@Override
 	protected void drawConnectingLine(Canvas canvas, float from, float to, Paint paint) {
-		float angle1 = (from / max) * ARC_SWEEP;
-		float angle2 = (to / max) * ARC_SWEEP;
+		float angle1 = (from / max) * mArcSweep;
+		float angle2 = (to / max) * mArcSweep;
 		float sweep = angle2 - angle1;
-		ArcUtils.drawArc(canvas, mCenter, mRadius, angle1 + ARC_START, sweep, mTrackOnPaint);
+		ArcUtils.drawArc(canvas, mCenter, mRadius, angle1 + mArcStart, sweep, mTrackOnPaint);
 	}
 
 	@Override
 	protected float getNearestBarValue(float x, float y) {
-		float normalized = getNormalizedAngle(x, y);
-		return normalized / ARC_SWEEP * max;
+		float normalized = (float) getNormalizedAngle(x, y);
+		return normalized / mArcSweep * max;
 	}
 
 	@Override
 	protected void getPointOnBar(PointF out, float value) {
-		float normalized = (value / max) * ARC_SWEEP;
-		out.set(ArcUtils.pointFromAngleDegrees(mCenter, mRadius, normalized + ARC_START));
+		float normalized = (value / max) * mArcSweep;
+		out.set(ArcUtils.pointFromAngleDegrees(mCenter, mRadius, normalized + mArcStart));
 	}
 
 	@Override
 	protected void drawBar(Canvas canvas, Paint paint) {
-		ArcUtils.drawArc(canvas, mCenter, mRadius, ARC_START, ARC_SWEEP, mTrackOffPaint);
+		ArcUtils.drawArc(canvas, mCenter, mRadius, mArcStart, mArcSweep, mTrackOffPaint);
 	}
 
 	// Private members /////////////////////////////////////////////////////////////
@@ -99,24 +125,24 @@ public class ArcSlider extends AbsSlider {
 	 * @param x, y
 	 * @return
 	 */
-	private float getAngle(float x, float y) {
-		return (float) Math.toDegrees(Math.atan2(y - mCenter.y, x - mCenter.x));
+	private double getAngle(float x, float y) {
+		return Math.toDegrees(Math.atan2(y - mCenter.y, x - mCenter.x));
 	}
 
 	/**
-	 * Returns the angle between ARC_START and the point.
+	 * Returns the angle between mArcStart and the point.
 	 *
 	 * @param x, y
 	 * @return
 	 */
-	private float getNormalizedAngle(float x, float y) {
-		float normalized = (getAngle(x, y) - ARC_START + 720f) % 360f;
+	private double getNormalizedAngle(float x, float y) {
+		double normalized = (getAngle(x, y) - mArcStart + 720f) % 360f;
 
-		if (normalized >= ARC_SWEEP / 2f + 180f) {
+		if (normalized >= mArcSweep / 2f + 180f) {
 			normalized = 0f;
 		}
-		else if (normalized > ARC_SWEEP) {
-			normalized = ARC_SWEEP;
+		else if (normalized > mArcSweep) {
+			normalized = mArcSweep;
 		}
 
 		return normalized;
